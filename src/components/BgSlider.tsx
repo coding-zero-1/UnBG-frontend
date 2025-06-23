67b3e36c8a36bca67018639fcf1df37e8df25a03
coding-zero-I <yashsangwan4700@gmail.com>
2025-06-23 20:41:10 +0530

    minor fix

--- src/components/BgSlider.tsx
+++ src/components/BgSlider.tsx
@@ -7,7 +7,7 @@ const BgSlider = () => {
         setSliderPosition(Number(event.target.value));
     }
   return (
-    <div className="pb-10 md:py-20 mx-2 ">
+    <div className="pb-10 md:py-5 mx-2 ">
         <h1 className="text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent mb-12 sm:mb-20">Remove background with <br /> high quality and accuracy</h1>
         <div className="relative w-full max-w-3xl overflow-hidden m-auto rounded-xl">
             <img src={assets.image_w_bg} alt="" style={{clipPath:`inset(0 ${100.2 - sliderPosition}% 0 0)`}} className="absolute top-0 left-0 w-full h-full" />
