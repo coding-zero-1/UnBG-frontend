971bf31afd4a1b78092507ddd54c09ad1b4028c3
coding-zero-I <yashsangwan4700@gmail.com>
2025-06-23 20:41:35 +0530

    added Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center mt-4">
+        <img src="./unbg-logo-navbar-2.png" alt="" className="w-28" />
+        <p>Copyright @YashSangwan | All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
