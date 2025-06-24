2ce99ecd16651d1a403ab5b047ef7eaa2348b37a
coding-zero-I <yashsangwan4700@gmail.com>
2025-06-24 20:24:23 +0530

    added clerk

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,25 @@
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 import {assets} from '../assets/assets';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const {openSignIn} = useClerk();
+  const { isSignedIn , user} = useUser();
   return (
     <div className="flex justify-between items-center mx-4 py-1 lg:mx-44">
         <Link to="/">
           <img src='/unbg-logo-navbar-2.png' alt="logo image" className="w-40 rounded-xl"/>
         </Link>
-        <button className='flex items-center cursor-pointer bg-zinc-800 gap-2 text-white px-4 py-2 rounded-full sm:px-8 sm:py-3 text-sm lg:text-base'>
+      { isSignedIn && user ? 
+        <div>
+          <UserButton />
+        </div> :
+        <button onClick={()=>openSignIn({})} className='flex items-center cursor-pointer bg-zinc-800 gap-2 text-white px-4 py-2 rounded-full sm:px-8 sm:py-3 text-sm lg:text-base'>
           <span>Get Started</span>
           <img src={assets.arrow_icon} alt="" className='w-3 sm:w-4' />
         </button>
+      }
+        
     </div>
   )
 }
